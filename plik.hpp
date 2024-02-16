993d8ae998919b3dbe12900b529f7ff14a9436b3
Dawid-Hel <155749831+Dawid-Hel@users.noreply.github.com>
2024-02-16 17:27:53 +0100

    plik.hpp

--- plik.hpp
+++ plik.hpp
@@ -4,4 +4,16 @@ void sort_bomb(int tablica[], int n);
 
 void funkcja(int tablica[], int n, int x, int y);
 
+void scal(int tab[], int lewy, int srodek, int prawy);
 
+void Sort_przez_wstrz( int tablica[], int size );
+
+void sort_przez_wst(int* tablica, int n);
+
+void quick_sort(int *tablica, int lewy, int prawy);
+
+int szuk_bin(int tablica[], int l, int p, int szukana);
+
+int szuk_liniowe(Element tablica[], int szuk_klucz, int rozmiar);
+
+int szuk_skok(int* lista, int n, int szukaj);
